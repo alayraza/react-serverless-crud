f14f2c2b39fd85b5f5adf40e0d5cd8b68d0bf459
alayraza <50444174+alayraza@users.noreply.github.com>
2020-11-27 16:35:59 +0500

    publish

---
+++ src/components/AppHeader/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../../assets/logo.svg'
+
+const AppHeader = (props) => {
+  return (
+    <header className='app-header'>
+      
+    </header>
+  )
+}
+
+export default AppHeader
